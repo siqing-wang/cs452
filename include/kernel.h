6705342e90956ad9f52b76a9160ddeaf3cbe4378
Yu Meng Zhang <ym4zhang@uwaterloo.ca>
2014-05-16 11:54:54 -0400

    Add memory address BASE/TOP in kernel.h

--- include/kernel.h
+++ include/kernel.h
@@ -7,6 +7,14 @@
 
 #include <context_switch.h>
 
+
+#define RAM_BASE 0x00000000
+#define RAM_TOP 0x01ffffff
+#define ROM_BASE 0x60000000
+#define RAM_TOP 0x607fffff
+#define DEVICE_REGISTERS_BASE 0x80800000
+#define DEVICE_REGISTERS_TOP 0x808fffff
+ 
 int Create(int priority, void (*code)());
 int MyTid();
 int MyParentTid();
