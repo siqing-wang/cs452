6eb590ccd87d8a07eb147abc8f8fa03eaf97fe58
Siqing Wang <sq3wang@uwaterloo.ca>
2014-05-19 23:26:03 -0400

    Add asm_lib for all assembly library code     -StoreRequestInA0

---
+++ include/asm_lib.h
@@ -0,0 +1,12 @@
+/*
+ * context_switch.h
+ */
+
+#ifndef __ASM_LIB_H__
+#define __ASM_LIB_H__
+
+#include <request.h>
+
+void storeRequestInR0(Request *request);
+
+#endif
---
+++ src/lib/asm_lib.asm
@@ -0,0 +1,19 @@
+	.file	"asm_lib.c"
+	.text
+	.align	2
+	.global	storeRequestInR0
+	.type	storeRequestInR0, %function
+storeRequestInR0:
+	@ args = 0, pretend = 0, frame = 4
+	@ frame_needed = 1, uses_anonymous_args = 0
+
+	mov	ip, sp
+	stmfd	sp!, {fp, ip, lr, pc}
+
+	mov r0, r0
+
+	sub	fp, ip, #4
+	ldmfd	sp, {fp, sp, pc}
+
+	.size	storeRequestInR0, .-storeRequestInR0
+	.ident	"GCC: (GNU) 4.0.2"
