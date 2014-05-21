2032e988b17239dd1b52588b7ef7fd0a04d12cdb
Siqing Wang <sq3wang@uwaterloo.ca>
2014-05-20 22:57:06 -0400

    Clean code for syscall.c

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -7,45 +7,45 @@
 #include <asm_lib.h>
 
 int Create(int priority, void (*code)()) {
-	Request request;
-	request.syscall = SYS_CREATE;
-	request.priority = priority;
-	request.code = code;
-	storeRequestInR0(&request);
+    Request request;
+    request.syscall = SYS_CREATE;
+    request.priority = priority;
+    request.code = code;
+    storeRequestInR0(&request);
     asm("swi");
-	register int retVal asm ("r0");
+    register int retVal asm ("r0");
     return retVal;
 }
 
 int MyTid() {
-	Request request;
-	request.syscall = SYS_MYTID;
-	storeRequestInR0(&request);
-	asm("swi");
-	register int retVal asm ("r0");
+    Request request;
+    request.syscall = SYS_MYTID;
+    storeRequestInR0(&request);
+    asm("swi");
+    register int retVal asm ("r0");
     return retVal;
 }
 
 int MyParentTid() {
-	Request request;
-	request.syscall = SYS_MYPID;
-	storeRequestInR0(&request);
-	asm("swi");
-	register int retVal asm ("r0");
+    Request request;
+    request.syscall = SYS_MYPID;
+    storeRequestInR0(&request);
+    asm("swi");
+    register int retVal asm ("r0");
     return retVal;
 }
 
 void Pass() {
-	Request request;
-	request.syscall = SYS_PASS;
-	storeRequestInR0(&request);
+    Request request;
+    request.syscall = SYS_PASS;
+    storeRequestInR0(&request);
     asm("swi");
 }
 
 void Exit() {
-	Request request;
-	request.syscall = SYS_EXIT;
-	storeRequestInR0(&request);
-	asm("swi");
+    Request request;
+    request.syscall = SYS_EXIT;
+    storeRequestInR0(&request);
+    asm("swi");
 }
 
