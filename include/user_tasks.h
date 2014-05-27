59731df058c7d51e67d9caeaebde18782b052be1
Siqing Wang <sq3wang@uwaterloo.ca>
2014-05-27 19:04:41 -0400

    Create a brief flow for user task

--- include/user_tasks.h
+++ include/user_tasks.h
@@ -5,7 +5,6 @@
 #ifndef __USER_TASKS_H__
 #define __USER_TASKS_H__
 
-void otherUserTask();
 void firstUserTask();
 
 #endif
--- src/tasks/user_tasks.c
+++ src/tasks/user_tasks.c
@@ -4,24 +4,38 @@
 
 #include <syscall.h>
 #include <bwio.h>
+#include <utils.h>
 
-void otherUserTask() {
-    bwprintf(COM2, "Tid: %d, Parent_Tid: %d.\n\r", MyTid(), MyParentTid());
-    Pass();
-    bwprintf(COM2, "Tid: %d, Parent_Tid: %d.\n\r", MyTid(), MyParentTid());
+void nameServer() {
+    bwprintf(COM2, "---NameServer Initialized---\n\r");
+    Exit();
+}
+
+void rpsServer() {
+    bwprintf(COM2, "---RPS Server Initialized---\n\r");
+    Exit();
+}
+
+void rpsClient() {
+    bwprintf(COM2, "---RPS Client Initialized---\n\r");
     Exit();
 }
 
 void firstUserTask() {
     int tid;
-    tid = Create(PRIORITY_LOW, &otherUserTask);
-    bwprintf(COM2, "Created: %d.\n\r", tid);
-    tid = Create(PRIORITY_LOW, &otherUserTask);
-    bwprintf(COM2, "Created: %d.\n\r", tid);
-    tid = Create(PRIORITY_HIGH, &otherUserTask);
-    bwprintf(COM2, "Created: %d.\n\r", tid);
-    tid = Create(PRIORITY_HIGH, &otherUserTask);
-    bwprintf(COM2, "Created: %d.\n\r", tid);
-    bwprintf(COM2, "First: exiting\n\r");
+    bwprintf(COM2, "---Bootstrap---\n\r");
+
+    // Create NameServer
+    tid = Create(PRIORITY_MED - 1, &nameServer);
+    assertEquals(NAMESERVER_TID, tid, "NameServer should be the first task.");
+
+    // Create RPS Server
+    tid = Create(PRIORITY_MED - 2, &rpsServer);
+
+    // Create RPS Client
+    tid = Create(PRIORITY_MED - 2, &rpsClient);
+
+
+    bwprintf(COM2, "---Program Finished---\n\r");
     Exit();
 }
