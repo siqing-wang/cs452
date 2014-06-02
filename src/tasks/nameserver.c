69e6c4f9dfe2e374134cda1466331be17881e870
Yu Meng Zhang <ym4zhang@uwaterloo.ca>
2014-06-02 12:03:00 -0400

    Modify nameserver. 	- Encapsulate hashtable detail.

--- src/tasks/nameserver.c
+++ src/tasks/nameserver.c
@@ -3,24 +3,87 @@
  */
 
 #include <nameserver.h>
-#include <server_linklist.h>
 #include <syscall.h>
 #include <utils.h>
 
 #define SERVICETABLE_MAX_SIZE 32
 
-void nameServer() {
-    ServerLinklistNode linklistTable[SERVICETABLE_MAX_SIZE];
-    ServerLinklistNode *serviceTable[HASH_TABLE_SIZE];
+/*
+ * server hashtable data structure.
+ */
+typedef struct NSHashNode {
+    char serverName[SERVERNAME_MAX_LENGTH];
+    int tid;
+    struct NSHashNode* next;
+} NSHashNode;
+
+void NSHashNode_init(NSHashNode *node, char *serverName, int tid) {
+    stringCopy(node->serverName, serverName, SERVERNAME_MAX_LENGTH);
+    node->tid = tid;
+    node->next = (NSHashNode*)0;
+}
+
+void ServerLinklist_pushFront(NSHashNode *list, NSHashNode *node) {
+    node->next = list;
+}
+
+NSHashNode* ServerLinklist_find(NSHashNode *list, char *serverName) {
+    for(;;) {
+        if (list == (NSHashNode*)0) {
+            return list;
+        } else if (stringEquals(list->serverName, serverName)) {
+            return list;
+        }
+        list = list->next;
+    }
+}
+
+typedef struct NSHash {
+    int serviceCount;
+    NSHashNode linklistTable[SERVICETABLE_MAX_SIZE];
+    NSHashNode *serviceTable[HASH_TABLE_SIZE];
+} NSHash;
+
+void NSHash_init(NSHash *hashtable) {
+    hashtable->serviceCount = 0;
     int i = 0;
     for(; i < HASH_TABLE_SIZE; i++) {
-        serviceTable[i] = (ServerLinklistNode *)0;
+        (hashtable->serviceTable)[i] = (NSHashNode *)0;
     }
+}
+
+NSHashNode* NSHash_get(NSHash *hashtable, char *name) {
+    int hash = computeHash(name);
+    NSHashNode* list = hashtable->serviceTable[hash];
+    return (NSHashNode*)ServerLinklist_find(list, name);
+}
+
+int NSHash_insert(NSHash *hashtable, char *name, int tid) {
+    int hash = computeHash(name);
+    NSHashNode* list = hashtable->serviceTable[hash];
+    if (hashtable->serviceCount >= SERVICETABLE_MAX_SIZE) {
+        return 0;
+    }
+    NSHashNode* newEntry = (NSHashNode *)(hashtable->linklistTable + hashtable->serviceCount);
+
+    NSHashNode_init(newEntry, name, tid);
+    ServerLinklist_pushFront(list, newEntry);
+    hashtable->serviceTable[hash] = newEntry;
+    hashtable->serviceCount++;
+    return 1;
+}
+
+/*
+ * NameServer
+ */
+void nameServer() {
+
+    NSHash hashtable;
+    NSHash_init(&hashtable);
 
     int tid;
-    int serviceCount = 0;
     NameserverMessage message;
-    char *serverName;
+    char *name;
     for(;;) {
         int byteSent = Receive(&tid, &message, sizeof(NameserverMessage));
         if (byteSent > sizeof(NameserverMessage)) {
@@ -28,30 +91,21 @@ void nameServer() {
             continue;
         }
 
-        serverName = message.serverName;
-        int hash = computeHash(serverName);
-        ServerLinklistNode* list = serviceTable[hash];
-        ServerLinklistNode* result = ServerLinklist_find(list, serverName);
-
+        NSHashNode* result = NSHash_get(&hashtable, name);
         int resultTid = -1;
-        if (result != (ServerLinklistNode *)0) {
-            resultTid = result->tid;
+        if (result != (NSHashNode *)0) {
+            resultTid =  result->tid;
         }
-
         switch (message.type) {
             int syscallResult;
             int successCode = NAMESERVER_SUCCESS;
             case NServerMSG_REGAS :
-                if (result != (ServerLinklistNode *)0) {
+                if (result != (NSHashNode *)0) {
                     result->tid = tid;
-                } else if (serviceCount < SERVICETABLE_MAX_SIZE - 1) {
-                    result = (ServerLinklistNode *)(linklistTable + serviceCount);
-                    serviceCount++;
-                    ServerLinklistNode_init(result, serverName, tid);
-                    ServerLinklist_pushFront(list, result);
-                    serviceTable[hash] = result;
                 } else {
-                    warning("Service Table Full.");
+                    if (!NSHash_insert(&hashtable, name, tid)) {
+                        warning("Service Table Full.");
+                    }
                 }
                 syscallResult = Reply(tid, &successCode, sizeof(int));
                 break;
